de3aee413fd2a25a3f48251369c0d82a3d17b6e3
rumyhuampas@gmail.com <rumyhuampas@gmail.com@edecb41b-7495-c900-c8ea-cadc82272756>
2012-06-24 07:54:00 +0000

    First Upload

---
+++ trunk/FT/FT/Controllers/TeamController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FT.Models;
+
+namespace FT.Controllers
+{
+    public class TeamController : Controller
+    {
+        private ftEntities db;
+
+        public TeamController()
+        {
+            db = new ftEntities();
+        }
+        //
+        // GET: /Team/
+
+        public ActionResult Index()
+        {
+            return View(db.teams.OrderBy(teams => teams.Name));
+        }
+
+        //
+        // GET: /Team/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Team/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Team/Create
+
+        [HttpPost]
+        public ActionResult Create(team teamObj)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                db.AddToteams(teamObj);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Team/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Team/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Team/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Team/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
